219ea128d17ecb7fde1bb84983eb27c69b145c0b
Meydson Baracho <64757536+meydson@users.noreply.github.com>
2025-08-11 20:14:46 -0300

    exercicio aula 3
    Exercício feito em sala

---
+++ Codigos_sala/aula3.rb
@@ -0,0 +1,29 @@
+valor = []
+total = 0
+produto_caro = 0
+produto_barato = 0
+
+until valor.last == 0 do
+  puts "Informe o valor do produto ou digite 0 para finalizar:"
+  valor.push(gets.chomp.to_f.round(2))
+end
+
+valor.pop
+produto_caro = valor[0]
+produto_barato = valor[0]
+
+
+for i in 0...valor.size do
+  total += valor[i]
+  puts = "Valor total #{total}"
+  if produto_caro < valor[i]
+    produto_caro = valor[i]
+  end
+  if produto_barato > valor[i]
+    produto_barato = valor[i]
+  end
+end
+
+puts "Produto mais caro: R$ #{produto_caro.round(2)}"
+puts "Produto mais barato: R$ #{produto_barato.round(2)}"
+puts "Valor total da lista: R$ #{total.round(2)}"
