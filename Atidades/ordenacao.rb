a018888c209dc203b3b901a3e93792f4a5b251e1
Meydson Baracho <64757536+meydson@users.noreply.github.com>
2025-08-12 10:49:28 -0300

    Exercicio 2
    Exercício de ordenação de array

---
+++ Atidades/ordenacao.rb
@@ -0,0 +1,8 @@
+array_ordenado = []
+
+[2, 10, 5, 30, 40].each do |numero|
+  posicao = array_ordenado.index { |x| x > numero } || array_ordenado.size
+  array_ordenado.insert(posicao, numero)
+end
+
+puts "Array ordenado: #{array_ordenado}"
